9c9a47398f4239deb43d5e7b86d295e5515444ad
Matthias Kreier <98946372+matthiaskreier@users.noreply.github.com>
2022-09-12 12:16:31 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+let message = "X"
+
+radio.setGroup(17)
+
+input.onButtonPressed(Button.A, function () {
+    message = "A"
+    radio.sendString(message)
+})
+
+input.onButtonPressed(Button.B, function () {
+    message = "B"
+    radio.sendString(message)
+})
+
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+
+basic.forever(function () {
+    radio.sendString(message)
+    basic.pause(10000)
+})

92ce715af36f2a43c23a75acf4fa77692aa7663b
Matthias Kreier <98946372+matthiaskreier@users.noreply.github.com>
2022-09-12 19:31:37 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,33 @@
-let message = "."
+// This is the sender
+
+let alphabet: string[] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let letter = 0
+let message = "R"
 
 radio.setGroup(17)
 
+basic.showString("S")
+
 input.onButtonPressed(Button.A, function () {
-    message = "A"
+    letter += 1
+    if (letter > 25) { letter = 0 }
+    message = alphabet[letter]
+    basic.showString(message)
     radio.sendString(message)
+    basic.showString("S")
 })
 
 input.onButtonPressed(Button.B, function () {
-    message = "B"
+    letter -= 1
+    if (letter < 0) {
+        letter = 25
+    }
+    message = alphabet[letter]
+    basic.showString(message)
     radio.sendString(message)
+    basic.showString("S")
 })
 
-radio.onReceivedString(function (receivedString) {
-    basic.showString(receivedString)
-})
 
 basic.forever(function () {
     radio.sendString(message)
